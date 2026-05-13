5b0f5bdcffeab9292763d60a4abd7d4d44af306a
Mamadou-Fall77 <serignepirefall@gmail.com>
2026-05-13 12:44:20 +0000

    Implement user age check and greeting
    Added a function to prompt user for name and age, and log messages based on age.

--- siteWeb.js
+++ siteWeb.js
@@ -2,3 +2,15 @@ window.addEventListener('scroll', function(){
   const header = document.querySelector('header');
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+function utilisateur() {
+  let nom = prompt("Entre votre nom :")
+  let age = number(prompt("Entrez votre age :"))
+
+if(age > 20) {
+  console.log("Autoriser" + nom)
+}else if(age === 20){
+  console.log("Vous avez de la chance" + nom)
+}else{
+  console.log("Désolé, vous n'etes pas autoriser" + nom)
+}
+utilisateur()
